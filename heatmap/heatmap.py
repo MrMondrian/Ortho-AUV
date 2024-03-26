617102fd9351922b285cc4afcab231c921640238
frompotenza <potenza.fontes@gmail.com>
2024-03-26 03:23:59 -0400

    ;

--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -1,3 +1,22 @@
-import numpy as np 
+import os
+from PIL import Image
+import numpy as np  # Assuming you want to save image data as NumPy arrays
 import quaternion
-import os 
+import json
+from argparse import ArgumentParser
+
+from heatmap_utils import *
+
+
+# Define the quaternion that rotates the from the AUV to the camera
+q_auv_cam = np.quaternion(0,0.707,-0.707,0)
+
+# returns q_nwu_cam given q_nwu_auv
+def fix_quat(q_nwu_auv):
+    return q_nwu_auv * q_auv_cam
+
+
+# Read data from "our_format.txt"
+with open(os.path.join(data_dir, "our_format.txt"), "r") as f:
+    lines = f.readlines()
+    lines = lines[1:]  # Skip the header line
--- heatmap/heatmap_utils.py
+++ heatmap/heatmap_utils.py
@@ -19,44 +19,48 @@ def fish_distance(img_depth):
      # distance to the fish will be average of the values within 1 std_dev
      return np.average(within_range)
 
-def fish_position(depth, x, y, z, quat):
+def fish_position(distance_to_fish, camera_position, camera_orientation):
      """
-     returns translation of the fish
+     returns position of the fish
 
      args:
-          depth (float): distance from the AUV to the fish
-          x, y, z (float): translation of AUV
-          quat (quaternion): orientation of AUV
+          distance_to_fish (float): distance from the camera to the fish
+          camera_position list[float]: position of AUV [x, y, z]
+          camera_orientation (quaternion): orientation of camera
      """
-     euler = quaternion.as_euler(quat)
-     roll, pitch = euler[0], euler[1]
-     fish_local_vec = []
-     fish_local_vec[0] = np.cos(roll)
-     fish_local_vec[1] = np.cos(pitch)
-     fish_local_vec[2] = depth
+     # convert quaternion angle to rotation matrix
+     rotation_matrix = quaternion.as_rotation_matrix(camera_orientation)
      
-     fish_global = quaternion.rotate(quat, fish_local_vec)
-
-     return fish_global
-      
+     # initial vector pointing in the direction of the positive X-axis
+     initial_vector = np.array([1, 0, 0])
      
+     # rotate the initial vector using the rotation matrix
+     local_fish_vector = np.dot(rotation_matrix, initial_vector)
+     
+     # scale the vector by the distance
+     local_fish_vector *= distance_to_fish
 
-def filter_fish_translations(fish_translations, translation_time):
+     global_fish_vector = local_fish_vector + camera_position
+     
+     return global_fish_vector
+    
+
+def filter_fish_positions(fish_positions, position_time):
      """
      returns a filtered/cleaned mapping of fish
 
      args:
-          fish_translations (list): list of translation (x, y, z) of every fish detected
-          translation_time (list): the time at which each translation was calculated
+          fish_positions (list): list of position (x, y, z) of every fish detected
+          position_time (list): the time at which each position was calculated
      """
      pass
 
-def make_heatmap(fish_translations):
+def make_heatmap(fish_positions):
      """
      returns a heatmap with fish distribution
 
      args:
-          fish_translations (list): filtered list of the translation (x, y, z) of every fish 
+          fish_positions (list): filtered list of the position (x, y, z) of every fish 
      """
      pass
 
@@ -69,3 +73,38 @@ def save_heatmap(heatmap, path):
           path (string): path to save the heatmap
      """
      pass
+
+
+# depth = 1
+# arr = [1, 1, 1]
+# quat_no_rotation = np.quaternion(1, 0, 0, 0)
+# quat_45_roll = np.quaternion(0.9238795, 0.3826834, 0, 0)
+# quat_90_roll = np.quaternion(0.7071068, 0.7071068, 0, 0)
+# quat_180_roll = np.quaternion(0, 1, 0, 0)
+# quat_45_pitch = np.quaternion(0.9238795, 0, 0.3826834, 0)
+# quat_90_pitch = np.quaternion(0.7071068, 0, 0.7071068, 0)
+# quat_180_pitch = np.quaternion(0, 0, 1, 0)
+# quat_45_yaw = np.quaternion(0.9238795, 0, 0, 0.3826834)
+# quat_90_yaw = np.quaternion(0.7071068, 0, 0, 0.7071068)
+# quat_180_yaw = np.quaternion(0, 0, 0, 1)
+
+# print(f"no rotation - {fish_position(depth, arr, quat_no_rotation)}")
+# print(f"45 roll - {fish_position(depth, arr, quat_45_roll)}")
+# print(f"90 roll - {fish_position(depth, arr, quat_90_roll)}")
+# print(f"180 roll - {fish_position(depth, arr, quat_180_roll)}")
+# print(f"45 pitch - {fish_position(depth, arr, quat_45_pitch)}")
+# print(f"90 pitch - {fish_position(depth, arr, quat_90_pitch)}")
+# print(f"180 pitch - {fish_position(depth, arr, quat_180_pitch)}")
+# print(f"45 yaw - {fish_position(depth, arr, quat_45_yaw)}")
+# print(f"90 yaw - {fish_position(depth, arr, quat_90_yaw)}")
+# print(f"180 yaw - {fish_position(depth, arr, quat_180_yaw)}")
+
+# quat_45_roll_45_yaw = np.quaternion(0.8535534, 0.3535534, -0.1464466, 0.3535534)
+# quat_90_roll_90_pitch = np.quaternion(0.5, 0.5, 0.5, 0.5)
+# quat_45_roll_45_pitch_45_yaw = np.quaternion(0.7325378, 0.4619398, 0.1913417, 0.4619398)
+
+# print(f"quat_45_roll_45_yaw - {fish_position(depth, arr, quat_45_roll_45_yaw)}")
+# print(f"quat_90_roll_90_pitch - {fish_position(depth, arr, quat_90_roll_90_pitch)}")
+# print(f"quat_45_roll_45_pitch_45_yaw - {fish_position(depth, arr, quat_45_roll_45_pitch_45_yaw)}")
+
+
