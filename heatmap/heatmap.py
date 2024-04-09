43e1572bb93b21f9f29bfed4c0700ac00c4a23f4
frompotenza <potenza.fontes@gmail.com>
2024-04-09 17:39:52 -0400

    fish coordinates looking better

--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -1,55 +1,59 @@
-import os
-from PIL import Image
 import numpy as np  # Assuming you want to save image data as NumPy arrays
 import quaternion
-import json
-from argparse import ArgumentParser
-import cv2
 from scipy.ndimage import gaussian_filter
-
+import cv2
 from heatmap_utils import *
 
 
+width, height, focal_x, focal_y, c_x, c_y = load_camera_params("data/cameras.json")
+x_over_z_map, y_over_z_map = camera_info(width, height, focal_x, focal_y, c_x, c_y)
+cam_position_offset = [0.2, 0.1, -0.2]
 
 predictions, repeat = load_predictions("vision/prediction.txt", "data/")
 poses = load_poses("data/out_format.txt", repeat)
-fish_distances = []
-for i in range(len(predictions)):
-    image_depth_path, x_0, x_1, y_0, y_1 = predictions[i]
-    fish_dist = fish_distance(image_depth_path, x_0, x_1, y_0, y_1)
-    fish_distances.append(fish_dist)
-    predictions[i].append(fish_dist)
-
-print(gaussian_filter(fish_distances, sigma=0.1))
-
-
 fish_positions = []
 
-for i in range(len(predictions)):
-    fish_dist = predictions[i][-1]
-    camera_position = poses[i][:3]
-    camera_orientation = poses[i][-1]
+def normalize(depth_map):
+    depth_map[np.isnan(depth_map)] = 0
 
-    fish_position = get_fish_position(fish_dist, camera_position, camera_orientation)
+    max_value = np.max(depth_map)
+    min_value = np.min(depth_map)
 
-    fish_positions.append(fish_position)
+    if max_value == min_value:
+        return np.zeros_like(depth_map)
+
+# Normalize array to range [0, 255]
+    normalized_depth = 255 * (depth_map - min_value) / (max_value - min_value)
     
+    
+    return normalized_depth.astype(np.uint8)
 
-# fish_positions = filter_fish_positions(fish_positions, ???position_time???)
-# 
-# fish_positions = np.array(fish_positions)
 
+
+for i in range(len(predictions)):
+    image_depth_path, x_0, x_1, y_0, y_1 = predictions[i]
+    z_map = np.loadtxt(image_depth_path)
+    point_cloud = get_xyz_image(z_map, width, height, x_over_z_map, y_over_z_map, cam_position_offset)
+    # print(np.min(point_cloud[:,:,2]))
+    # print(np.max(point_cloud))
+    point_cloud = crop_to_box(point_cloud, x_0, x_1, y_0, y_1)
+    # print(point_cloud)
+    point_cloud = clean_point_cloud(point_cloud)
+
+    normalized_depth = normalize(point_cloud)
+    cv2.imwrite(f"normalized_image{i}.png", normalized_depth)
+    x, y, z = get_fish_position(point_cloud, poses[i])
+    fish_positions.append([x, y, z])
+
+print(fish_positions)
 auv_positions = []
 
 for pose in poses:
     auv_positions.append(pose[:3])
     
-auv_positions = np.array(auv_positions)
 fish_positions = np.array(fish_positions)
 
-heatmap_size = get_heatmap_size(fish_positions, auv_positions) 
-
-make_heatmap(fish_positions, heatmap_size)
+calculate_and_plot_cluster(fish_positions)
 
 
 
--- heatmap/heatmap_utils.py
+++ heatmap/heatmap_utils.py
@@ -3,9 +3,67 @@ import quaternion
 from mpl_toolkits.mplot3d import Axes3D 
 import matplotlib.pyplot as plt 
 from sklearn.cluster import DBSCAN
+from scipy.stats import gaussian_kde
 from pylab import *
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
+import json
+from argparse import ArgumentParser
+
+
+def load_camera_params(path_to_camera_params):
+     """ 
+     load camera params from json file
+
+     args: 
+          path_to_camera_params (string): path to camera params json file
+
+     returns:
+          width (int): width of camera fov 
+          height (int): height of camera fov 
+          focal_x (float): focal point x-coordinate
+          focal_y (float): focal point y-coordinate  
+          c_x (float): 
+          c_y (float):
+     """
+     with open(path_to_camera_params, "r") as f:
+          camera_data = json.load(f)
+
+     # Extract the camera intrinsics parameters
+     camera_params = list(camera_data.values())[0]
+     width = camera_params['width']
+     height = camera_params['height']
+     focal_x = camera_params['focal_x']
+     focal_y = camera_params['focal_y']
+     c_x = camera_params['c_x']
+     c_y = camera_params['c_y']
+
+     return width, height, focal_x, focal_y, c_x, c_y
+
+
+def camera_info(width, height, focal_x, focal_y, c_x, c_y):
+     """ 
+     calculate camera info
+
+     args: 
+          width (int): width of camera fov 
+          height (int): height of camera fov 
+          focal_x (float): focal point x-coordinate
+          focal_y (float): focal point y-coordinate  
+          c_x (float): 
+          c_y (float):
+     
+     returns:
+          x_over_z_map
+          y_over_z_map
+     """
+     u_map = np.tile(np.arange(width), (height, 1)) + 1
+     v_map = np.tile(np.arange(height), (width, 1)).T + 1
+
+     x_over_z_map = (c_x - u_map) / focal_x
+     y_over_z_map = (c_y - v_map) / focal_y
+
+     return x_over_z_map, y_over_z_map
 
 
 def load_predictions(path_to_predictions_file, path_to_depth_folder):
@@ -16,7 +74,7 @@ def load_predictions(path_to_predictions_file, path_to_depth_folder):
           path_to_predictions (string): path to prediction text file
 
      return:
-          predictions (list): [[x_1, y_1, width_1, height_1], ... for all instances]
+          predictions (list): [[path_to_depth_file, x_1, y_1, width_1, height_1], ... for all detection instances]
           repeat (dict): number of detected fish per image
      """
      with open(path_to_predictions_file, "r") as f:
@@ -50,6 +108,7 @@ def load_predictions(path_to_predictions_file, path_to_depth_folder):
      
      return predictions, repeat
 
+
 def load_poses(path_to_poses, repeat):
      """ 
      returns a list with all the auv poses for each prediction
@@ -82,63 +141,108 @@ def load_poses(path_to_poses, repeat):
           qz = float(qz)
 
           quat = np.quaternion(qw, qx, qy, qz)
-          quat = fix_quat(quat)
+          # quat = fix_quat(quat)
 
           for i in range(n):
                poses.append([x, y, z, quat])
      
      return poses
 
-def fish_distance(image_depth_path, x_0, x_1, y_0, y_1):
-     """ NOT TESTED - ARRAY SLICING MIGHT BE WRONG """
-     """ 
-     returns the distance from the camera to the fish 
 
-     args:
-          img (float): path to image depth 
-          x_0, x_1, y_0, x_1 (int, int, int, int): bounding box coordinates
-     """
-     # load depth map
-     depth_map = np.loadtxt(image_depth_path)
+def get_xyz_image(z_map, width, height, x_over_z_map, y_over_z_map, cam_position_offset):
+     cam_x_offset, cam_y_offset, cam_z_offset = cam_position_offset
+     xyz_img = np.zeros((height, width, 3))
 
-     # select only bounding box
-     fish_depth_cut = depth_map[y_0 : y_1 + 1, x_0 : x_1 + 1]
+     x_map = x_over_z_map * -z_map
+     y_map = y_over_z_map * -z_map
 
-     # Find min value of the image and get distance points within 1 std_dev of min_value
-     min_value = np.min(fish_depth_cut)
-     std_dev = np.std(fish_depth_cut)
-     min_range = min_value + std_dev
-     max_range = min_value - std_dev
+     xyz_img[:, :, 0] = y_map + cam_x_offset
+     xyz_img[:, :, 1] = x_map + cam_y_offset
+     xyz_img[:, :, 2] = -z_map + cam_z_offset
 
-     within_range = fish_depth_cut[(fish_depth_cut >= max_range) & (fish_depth_cut <= min_range)]
+     return xyz_img
 
-     # distance to the fish will be average of the values within 1 std_dev
-     return np.average(within_range)
 
-def get_fish_position(distance_to_fish, camera_position, camera_orientation):
-     """
-     returns position of the fish
+def crop_to_box(img, x_0, x_1, y_0, y_1):
+     return img[y_0:y_1, x_0:x_1]
 
-     args:
-          distance_to_fish (float): distance from the camera to the fish
-          camera_position list[float]: position of AUV [x, y, z]
-          camera_orientation (quaternion): orientation of camera
-     """
-     # convert quaternion angle to rotation matrix
-     rotation_matrix = quaternion.as_rotation_matrix(camera_orientation)
+
+def clean_point_cloud(point_cloud):
+     # Find the closest point to the camera
+     initial_point_cloud_shape = point_cloud.shape
+     point_cloud = point_cloud.reshape(-1,3)
+     point_cloud[point_cloud[:,2] > -0.3] = -10000 # ignore depth values which are less than 0.5m
+     closest_point_index = np.argmax(point_cloud[:,2])
      
-     # initial vector pointing in the direction of the positive X-axis
-     initial_vector = np.array([1, 0, 0])
-     
-     # rotate the initial vector using the rotation matrix
-     local_fish_vector = np.dot(rotation_matrix, initial_vector)
-     
-     # scale the vector by the distance
-     local_fish_vector *= distance_to_fish
+     eps = 0.3  # Maximum distance between two samples for them to be considered as in the same neighborhood
+     min_samples = 10  # The number of samples in a neighborhood for a point to be considered as a core point
 
-     global_fish_vector = local_fish_vector + camera_position
+     # Perform DBSCAN clustering
+     dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+     labels = dbscan.fit_predict(point_cloud)
      
-     return global_fish_vector
+     # Extract the cluster containing the seed point
+     closest_object_label = labels[closest_point_index]
+     object_mask = (labels == closest_object_label)
+     point_cloud[~object_mask] = np.array([np.nan]*3)
+     
+     point_cloud = point_cloud.reshape(initial_point_cloud_shape)
+     object_mask = object_mask.reshape(initial_point_cloud_shape[0:2])
+     
+     return point_cloud
+
+def get_fish_position(point_cloud, auv_pose):
+     auv_x, auv_y, auv_z, auv_quat = auv_pose
+
+     min_lx = np.nanmin(point_cloud[:,:,0].flatten())
+     min_ly = np.nanmin(point_cloud[:,:,1].flatten())
+     min_lz = np.nanmin(point_cloud[:,:,2].flatten())
+     max_lx = np.nanmax(point_cloud[:,:,0].flatten())
+     max_ly = np.nanmax(point_cloud[:,:,1].flatten())
+     max_lz = np.nanmax(point_cloud[:,:,2].flatten())
+     
+     lx = (max_lx + min_lx) / 2
+     ly = (max_ly + min_ly) / 2
+     lz = (max_lz + min_lz) / 2
+
+     print(lx, ly, lz)
+     # print(ly)
+     # print(lz)
+     
+     global_obj_pos_offset = quaternion.rotate_vectors(auv_quat, np.array([lx,ly,lz]))
+     
+     # Get the best estimate of the mean
+     x,y,z = global_obj_pos_offset + np.array([auv_x, auv_y, auv_z])
+
+     return x, y, z
+
+# def get_fish_position(prediction, distance_to_fish, camera_position, camera_orientation):
+#      """
+#      returns position of the fish
+
+#      args:
+#           prediction (list): [path_to_depth_file, x_1, y_1, width_1, height_1]
+#           distance_to_fish (float): distance from the camera to the fish
+#           camera_position list[float]: position of AUV [x, y, z]
+#           camera_orientation (quaternion): orientation of camera
+#      """
+#      _, x, y, width, height = prediction
+
+#      # convert quaternion angle to rotation matrix
+#      rotation_matrix = quaternion.as_rotation_matrix(camera_orientation)
+     
+#      # initial vector pointing in the direction of the positive X-axis
+#      initial_vector = np.array([1, 0, 0])
+     
+#      # rotate the initial vector using the rotation matrix
+#      local_fish_vector = np.dot(rotation_matrix, initial_vector)
+     
+#      # scale the vector by the distance
+#      local_fish_vector *= distance_to_fish
+
+#      global_fish_vector = local_fish_vector + camera_position
+     
+#      return global_fish_vector
     
 
 def filter_fish_positions(fish_positions):
@@ -149,6 +253,7 @@ def filter_fish_positions(fish_positions):
           fish_positions (list): list of position [x, y, z] of every fish detected
      """
      pass
+
 
 def calculate_and_plot_cluster(fish_positions):
      """ 
@@ -230,6 +335,7 @@ def get_heatmap_size(fish_positions, auv_positions):
 
      return axis_size
 
+
 def make_heatmap(fish_positions, heatmap_size):
      """
      returns a heatmap with fish distribution
@@ -238,13 +344,13 @@ def make_heatmap(fish_positions, heatmap_size):
           fish_positions (list): filtered list of the position [x, y, z] of every fish 
           auv_positions (list): list of all auv positions [x, y, z] 
      """ 
-     kde = gaussian_kde(data.T)
+     kde = gaussian_kde(fish_positions.T)
 
      # Define grid for plotting
      x_grid, y_grid, z_grid = np.meshgrid(
-          np.linspace(data[:, 0].min(), data[:, 0].max(), 50),
-          np.linspace(data[:, 1].min(), data[:, 1].max(), 50),
-          np.linspace(data[:, 2].min(), data[:, 2].max(), 50)
+          np.linspace(fish_positions[:, 0].min(), fish_positions[:, 0].max(), 50),
+          np.linspace(fish_positions[:, 1].min(), fish_positions[:, 1].max(), 50),
+          np.linspace(fish_positions[:, 2].min(), fish_positions[:, 2].max(), 50)
      )
 
      density = kde(np.vstack([x_grid.ravel(), y_grid.ravel(), z_grid.ravel()]))
